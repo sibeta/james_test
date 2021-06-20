cc87017b77b3036ad93c3af37ace9414b3cd97a6
sibeta <1428978614@qq.com>
2021-06-20 20:02:21 +0800

    optimize

--- src/com/james/test/Solution.java
+++ src/com/james/test/Solution.java
@@ -1,5 +1,8 @@
 package com.james.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,37 +10,71 @@ import java.util.regex.Pattern;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println("****************************************************");
-        System.out.println("Do you want to read file for initialization?(yes/no)");
+        doTrade();
+    }
 
+    private static void doTrade() {
         String input = "";
-        boolean hasShowQuitHint = false;
-        StringBuilder sb = new StringBuilder();
+        boolean outputResultFinished = false;
+        Scanner scanner = new Scanner(System.in);;
+        HashMap<String, Integer> resultMap = new HashMap<>();
+
+        showWelcomeHint();
+        showIfInitialHint();
+
         while (true) {
-            Scanner scan = new Scanner(System.in);
-            input = scan.nextLine();
+            input = scanner.nextLine();
             if(isYesStr(input)) {
                 showInitialHint();
+                String initialData = initTradeFromFile(resultMap);
+                showInitialData(initialData);
+                showInputMoreHint();
             }
 
             if(isQuitCmd(input)) {
+                if (scanner != null) {
+                    scanner.close();
+                }
                 break;
             }
 
             if(triggerExit(input)) {
-                if(!hasShowQuitHint) {
-                    outputResult(sb.toString());
+                if(!outputResultFinished) {
+                    showOutputHint();
+                    outputResult(resultMap);
+                    outputResultFinished = true;
                 }
 
                 showQuitHint();
-                hasShowQuitHint = true;
             }
 
             if(isFormatValid(input)) {
-                sb = sb.append(input).append("\n");
+                putToResultMap(resultMap, input);
                 showInputMoreHint();
             }
         }
+    }
+
+    private static String initTradeFromFile(HashMap<String, Integer> resultMap) {
+        Scanner scanner = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            scanner = new Scanner(new File("./trade.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line = "";
+        while(scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            sb.append(line).append("\n");
+            putToResultMap(resultMap, line);
+        }
+
+        if (scanner != null) {
+            scanner.close();
+        }
+
+        return sb.toString();
     }
 
     private static boolean isYesStr(String input) {
@@ -61,19 +98,53 @@ public class Solution {
     }
 
     private static boolean isFormatValid(String input) {
-        String pattern = "[A-Z]{3}\\s[1-9]{1}[0-9]{2}";
+        String pattern = "[A-Z]{3}\\s-?[1-9][0-9]*";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(input);
         return m.matches();
     }
 
-    private static void outputResult(String result) {
-        System.out.println("Output:");
+    private static void putToResultMap(HashMap<String, Integer> resultMap, String line) {
+        String[] lineArray = line.split(" ");
+        if(lineArray.length == 2) {
+            String key = lineArray[0];
+            Integer val = Integer.parseInt(lineArray[1]);
+            if(resultMap.containsKey(key)) {
+                val = resultMap.get(key) + val;
+            }
+            resultMap.put(key, val);
+        }
+    }
+
+    private static void outputResult(HashMap<String, Integer> resultMap) {
+        String result = transferMapToString(resultMap);
         System.out.println(result);
+    }
+
+    private static String transferMapToString(HashMap<String, Integer> resultMap) {
+        StringBuilder sb = new StringBuilder();
+        for(String key : resultMap.keySet()) {
+            sb = sb.append(key).append(" ").append(resultMap.get(key)).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static void showWelcomeHint() {
+        System.out.println("****************************************************");
+        System.out.println("***********Welcome to the trade center!*************");
+        System.out.println("****************************************************");
+    }
+
+    private static void showIfInitialHint() {
+        System.out.println("Do you want to read file for initialization?(yes/no)");
     }
 
     private static void showInitialHint() {
         System.out.println("Initial payment inputs");
+    }
+
+    private static void showInitialData(String data) {
+        System.out.println(data);
     }
 
     private static void showInputMoreHint() {
@@ -83,4 +154,9 @@ public class Solution {
     private static void showQuitHint() {
         System.out.println("Type quit anytime if you want to quit the program");
     }
+
+    private static void showOutputHint() {
+        System.out.println("Output:");
+    }
+
 }
